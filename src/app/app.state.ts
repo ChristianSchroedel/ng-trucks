f157708d0bc504f172d15d90c19ba9b1844e873b
Christian Schrödel <schroedel.christian@googlemail.com>
2016-09-23 13:30:27 +0200

    Removed obsolete state

--- src/app/app.state.ts
+++ src/app/app.state.ts
@@ -3,10 +3,8 @@
  */
 import {LoadedEventsState} from './food-trucks/reducers/loaded-events.reducer';
 import {Operator} from './food-trucks/services/foodtruck.service';
-import {ScreenState} from './reducers/screen.reducer';
 
 export interface AppState {
   loadedEvents: LoadedEventsState,
-  loadedOperators: Operator[],
-  currentScreen: ScreenState
+  loadedOperators: Operator[]
 }
